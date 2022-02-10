f027f206b75e54953d15444b2dd310702e352cf2
Jeremy Burks <jeremyburks@Ravens-MacBook-Pro.local>
2022-02-10 12:24:04 -0800

    This is a personal script

---
+++ hello.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+network_info=$(ifconfig)
+
+echo "who are you?"
+read name
+echo "Hello, $name."
+echo "what are you doing here?"
+read coding
+echo "It's DANGEROUS to go alone, take this network info with you."
+echo $network_info
