d07548db14d73ae3cf4b137da9332e2e14116ee3
Jeremy Burks <jeremygburks@gmail.com>
2022-02-15 19:28:44 -0800

    new file addition

---
+++ concant.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VAR=""
+for ELEMENT in 'Hydrogen' 'Helium' 'Lithium' 'Beryllium'; do
+  VAR+="${ELEMENT} "
+done
+
+echo "$VAR"
+
+    VAR1="Hello, "
+    VAR1+=" World"
+    echo "$VAR1"
