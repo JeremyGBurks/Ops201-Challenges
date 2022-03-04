cf908926523daac76d948c7028675ff8abad924a
jeremyGBurks <jeremygburks.Gmail.com>
2022-03-04 03:29:46 +0000

    changed syntax for comments

--- Robocopy.bat
+++ Robocopy.bat
@@ -1,11 +1,11 @@
-# Script:                       Ops Challenge 08 (robocopy)
-# Author:                       Jeremy Burks
-# Date of latest revision:      03/05/22
-# Purpose:                      Automates the copy function of files on the target computer. Windows Task Scheduler was used to set this to occur every night at 21:00hrs.
+: Script:                       Ops Challenge 08 (robocopy)
+: Author:                       Jeremy Burks
+: Date of latest revision:      03/05/22
+: Purpose:                      Automates the copy function of files on the target computer. Windows Task Scheduler was used to set this to occur every night at 21:00hrs.
 
 
 @echo off
 
 ROBOCOPY C:\users\j.thompson\Desktop\MyProjectFiles E:\Desktop /LOG:backuplog.txt
 
-pause
+@pause
