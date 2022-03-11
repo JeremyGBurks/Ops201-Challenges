da3926e624f211a5e190b060e9cd18cdc128798f
jeremyGBurks <jeremygburks.Gmail.com>
2022-03-11 03:33:39 +0000

    changes made to comments

--- LogRetrieval.ps1
+++ LogRetrieval.ps1
@@ -18,19 +18,17 @@ $event = Get-EventLog -LogName System -After (Get-Date).AddDays(-1)
 $event | Out-File -FilePath $DesktopPath\last_24.txt
 
 "`n"
-echo "Creating file with 100 most recent errors"  
+echo "Creating file at $DesktopPath with 100 most recent errors"  
 $eventnewest100 = Get-EventLog -LogName System -EntryType Error -Newest 100 
 $eventnewest100 | Out-File -FilePath $DesktopPath\errors.txt
 
 "`n"
-echo "100 Most recent events with Event ID 16: "
-$eventlog16 = Get-EventLog -LogName System -InstanceId 16 -Newest 100
-$eventlog16
+echo "All recent events with Event ID 16: "
+Get-EventLog -LogName System -InstanceId 16 
 
 "`n"
 echo "20 Most recent entries in system event log: "
-$eventnew20 = Get-EventLog -LogName System -Newest 20
-$eventnew20
+Get-EventLog -LogName System -Newest 20
 
 "`n"
 echo "Sources of most recent 500 events: "
