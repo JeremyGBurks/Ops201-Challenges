7b03e92ecba139d7993f0cd69cd807e409f92629
Jeremy Burks <97761340+JeremyGBurks@users.noreply.github.com>
2022-03-10 20:29:39 -0800

    Update getprocess.ps1
    Added echo for the first three commands and changed language in descriptions

--- getprocess.ps1
+++ getprocess.ps1
@@ -1,26 +1,28 @@
 # Script:                       Ops Challenge 10 (PowerShell Get-Process)
 # Author:                       Jeremy Burks
 # Date of latest revision:      03/09/22
-# Purpose:                      sorts and displays specified processes. Starts iexplore as well as uses a <for> loop to open 10 instances. Cmdlt to shut down iexplore, and an interactive menu to prompt user for PID to close program
+# Purpose:                      Sorts and displays specified processes. Starts iexplore as well as uses a <for> loop to open 10 instances. Cmdlt to shut down iexplore, and an interactive menu to prompt user for PID to close program
 
-#Line 14 Sets Get-Process to the variable $G
-#Line 16 Sorts CPU process in descending order from highest to lowest
-#Line 18 Sorts PIDs in descending order
-#Line 20 Sorts working sets in descending order but specifies only the top 5
-#Line 22 Opens iexplore to a particular web page
-#Lines 25 through 27 are a <for> loop that uses logic to open 10 instances of iexplorer
-#Line 29 Kills iexplore
-#Line 31 through 33 Prints out processes in descending order by their PID then prompts the user to enter a PID to terminate a prcess
+#Line 15 Sets Get-Process to the variable $G
+#Line 17 Sorts CPU process in descending order from highest to lowest
+#Line 20 Sorts PIDs in descending order
+#Line 23 Sorts working sets in descending order but specifies only the top 5
+#Line 25 Opens iexplore to a particular web page
+#Line 27 through 29 is a <for> loop that uses logic to open 10 instances of iexplorer
+#Line 31 Kills iexplore
+#Line 33 through 35 Prints out processes in descending order by their PID then prompts the user to enter a PID to terminate a prcess
+
 $G = Get-Process
-
+echo "Active CPU Processes" 
 $G | Sort-Object -Property CPU -Descending 
 "`n"
+echo "Active Processes sorted by PID in descending order"
 $G | Sort-Object -Property ID -Descending
 "`n"
+echo "Top 5 active working sets in descending order"
 $G | Sort-Object -Property WS -Descending | Select -First 5
 "`n"
 [System.Diagnostics.Process]::Start("iexplore","https://owasp.org/www-project-top-ten/")
-"`n"
 
 for ($Startproc=1; $Startproc -le 10; $Startproc++){
     [System.Diagnostics.Process]::Start("iexplore","https://owasp.org/www-project-top-ten/")
